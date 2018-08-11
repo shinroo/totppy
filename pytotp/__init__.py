fb25584729a3e310b12e8c375fbc415484d0a166
Abdullah Selek <abdullahselek@gmail.com>
2018-08-11 22:10:58 +0100

    Add init file for module.

---
+++ pytotp/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""A Python library for generating TOTP and HOTP one-time passwords."""
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/pytotp'
+__download_url__ = 'https://github.com/abdullahselek/pytotp'
+__description__  = 'A Python library for generating TOTP and HOTP one-time passwords.'
