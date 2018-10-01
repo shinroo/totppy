0f4f87ee8621985968600275192394fc498cc9b8
Abdullah Selek <abdullahselek@gmail.com>
2018-10-01 13:30:20 +0100

    Add docs on base32helper.

--- pytotp/base32helper.py
+++ pytotp/base32helper.py
@@ -3,8 +3,22 @@
 
 import base64
 
+"""Encodes given str with base32.
+Args:
+  str_to_encode (str):
+    str to be encoded.
+Returns:
+  Base32 encoded bytes.
+"""
 def encode(str_to_encode):
     return base64.b32encode(str.encode(str_to_encode))
 
+"""Decodes given bytes with base32.
+Args:
+  bytes_to_decode (bytes):
+    bytes to be decoded.
+Returns:
+  Base32 decoded str.
+"""
 def decode(bytes_to_decode):
     return base64.b32decode(bytes_to_decode).decode('utf-8')
