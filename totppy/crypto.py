d5f7441825ddd0dfaa2ea21d9bfa9a169d57393f
Abdullah Selek <abdullahselek@gmail.com>
2018-11-07 18:15:11 +0000

    Fix for crypto documentation.

--- totppy/crypto.py
+++ totppy/crypto.py
@@ -7,16 +7,17 @@ import binascii
 
 from .enums import Algorithm
 
-"""Return a new hmac object.
-Args:
-  algorithm (enum):
-    Enum type from `Algorithm`.
-  key (str):
-    Key for the keyed hash object.
-  data (bytes):
-    Initial input for the hash, if provided.
-"""
 def HMAC(algorithm, key, data):
+      """Return a new hmac object.
+      Args:
+        algorithm (enum):
+          Enum type from `Algorithm`.
+        key (str):
+          Key for the keyed hash object.
+        data (bytes):
+          Initial input for the hash, if provided.
+      """
+
     byte_key = key.encode()
     if algorithm == Algorithm.SHA1:
         return hmac.new(byte_key, msg=data, digestmod=hashlib.sha1).hexdigest().upper()
