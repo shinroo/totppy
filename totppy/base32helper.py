45e3e66b96c8f054cbf9f1782814d92f9ea3c8f4
Abdullah Selek <abdullahselek@gmail.com>
2018-11-07 18:16:49 +0000

    Fixed documentation on base32 helper.

--- totppy/base32helper.py
+++ totppy/base32helper.py
@@ -3,22 +3,24 @@
 
 import base64
 
-"""Encodes given str with base32.
-Args:
-  str_to_encode (str):
-    str to be encoded.
-Returns:
-  Base32 encoded bytes.
-"""
 def encode(str_to_encode):
+    """Encodes given str with base32.
+    Args:
+      str_to_encode (str):
+        str to be encoded.
+    Returns:
+      Base32 encoded bytes.
+    """
+
     return base64.b32encode(str.encode(str_to_encode))
 
-"""Decodes given bytes with base32.
-Args:
-  bytes_to_decode (bytes):
-    bytes to be decoded.
-Returns:
-  Base32 decoded str.
-"""
 def decode(bytes_to_decode):
+    """Decodes given bytes with base32.
+    Args:
+      bytes_to_decode (bytes):
+        bytes to be decoded.
+    Returns:
+      Base32 decoded str.
+    """
+
     return base64.b32decode(bytes_to_decode).decode('utf-8')
