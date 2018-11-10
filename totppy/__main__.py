89345cfec69c1c4c71c663b9ca92f6b1ae84e8f8
Abdullah Selek <abdullahselek@gmail.com>
2018-11-10 10:58:37 +0000

    Start from help message.

--- totppy/__main__.py
+++ totppy/__main__.py
@@ -11,3 +11,17 @@ from totppy import (
     Algorithm,
     Token
 )
+
+help_message = '''
+  A Python library for generating TOTP and HOTP one-time passwords.
+  Usage
+    $ python totppy [<options>]
+  Options
+    --help, -h              Display help message
+    --version, -v           Display installed version
+  Examples
+    $ python spampy --help
+    $ python spampy --version
+'''
+
+spampy_version = __version__
