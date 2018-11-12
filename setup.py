fa3eca9a909bee1897729a274aad694782308c42
Abdullah Selek <abdullahselek@gmail.com>
2018-11-12 20:50:39 +0000

    Update dependencies in setup.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     download_url=extract_metaitem('download_url'),
     packages=find_packages(exclude=('tests', 'docs')),
     platforms=['Any'],
-    install_requires=['enum34'],
+    install_requires=['enum34', 'click'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     keywords='totp, hotp, one-time passwords, unique tokens, totp token creater',
