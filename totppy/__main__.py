39773f6d1c6ae1d1428fd8a10a50c7c494bf6358
Abdullah Selek <abdullahselek@gmail.com>
2018-11-16 13:42:32 +0000

    Complete cli.

--- totppy/__main__.py
+++ totppy/__main__.py
@@ -20,28 +20,34 @@ help_message = '''
   Options
     --help, -h              Display help message
     --version, -v           Display installed version
-    --generator, -g         Create one time password with generator.
-    --token, -t             Create one time password with token.
+    --generator, -g         Create one time password with generator, prompts for inputs.
+    --token, -t             Create one time password with token, prompts for inputs.
   Examples
     $ python totppy --help
     $ python totppy --version
-    $ python totppy --generator 6 15 'secret'
-    $ python totppy --token 6 15 'secret' 'name' 'issuer'
+    $ python totppy --generator
+    $ python totppy --token
 '''
 
 totppy_version = __version__
 
 @click.command(add_help_option=False)
-@click.option('-h', '--help', is_flag=True, default=False, help='Display help message.')
-@click.option('-v', '--version', is_flag=True, default=False, help='Display installed version.')
+@click.option('-h', '--help',
+              is_flag=True,
+              default=False,
+              help='Display help message.')
+@click.option('-v', '--version',
+              is_flag=True,
+              default=False,
+              help='Display installed version.')
 @click.option('-g', '--generator',
-              help='Create one time password with generator.',
-              default=[None] * 3,
-              type=(int, int, str))
+              is_flag=True,
+              default=False,
+              help='Create one time password with generator, prompts for inputs.')
 @click.option('-t', '--token',
-              help='Create one time password with token.',
-              default=[None] * 5,
-              type=(int, int, str, str, str))
+              is_flag=True,
+              default=False,
+              help='Create one time password with token, prompts for inputs.')
 
 def main(help, version, generator, token):
     if (help):
@@ -51,13 +57,19 @@ def main(help, version, generator, token):
         if (version):
             print('totppy' +  ' ' + totppy_version)
         elif (generator):
-            (digits, period, secret) = generator
+            digits = click.prompt('Digits', type=int)
+            period = click.prompt('Period', type=int)
+            secret = click.prompt('Secret', type=str)
             factor = Factor(digits=digits, period=period)
             generator = Generator(factor, Algorithm.SHA1, secret)
             password = generator.password(datetime.datetime.now())
             print('One time password : ' + str(password))
         elif (token):
-            (digits, period, secret, name, issuer) = token
+            digits = click.prompt('Digits', type=int)
+            period = click.prompt('Period', type=int)
+            secret = click.prompt('Secret', type=str)
+            name = click.prompt('Name', type=str)
+            issuer = click.prompt('Issuer', type=str)
             factor = Factor(digits=digits, period=period)
             generator = Generator(factor, Algorithm.SHA1, secret)
             token = Token(generator, name, issuer)
